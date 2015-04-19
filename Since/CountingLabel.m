b559ec79a3b68491e85557d57dbc7f8ae61ef8eb
shanecarey17 <shanecarey17@gmail.com>
2015-04-19 14:10:23 -0500

    refactored timer to use timer user data

--- Since/CountingLabel.m
+++ Since/CountingLabel.m
@@ -11,11 +11,6 @@
 @interface CountingLabel ()
 {
     NSTimer *timer;
-    NSInteger value;
-    NSInteger startValue;
-    NSInteger endValue;
-    Float32 progress;
-    Float32 duration;
 }
 
 @end
@@ -23,58 +18,63 @@
 @implementation CountingLabel
 
 - (void)countToValue:(NSInteger)end duration:(CGFloat)time timing:(CountingLabelTimingFunction)function {
-    [self countFromValue:value toValue:end duration:time timing:function];
+    [self countFromValue:[self.text integerValue] toValue:end duration:time timing:function];
 }
 
 - (void)countFromValue:(NSInteger)start toValue:(NSInteger)end duration:(CGFloat)time timing:(CountingLabelTimingFunction)function {
     // Disable previous timer
     [timer invalidate];
     
-    // Set values
-    value = startValue = start;
-    endValue = end;
-    progress = 0.0f;
-    duration = time;
-    
     // Begin
-    NSDictionary *timingFunction = @{@"timingFunction" : @(function)};
+    NSMutableDictionary *timingFunction = [[NSMutableDictionary alloc] initWithDictionary:@{@"timingFunction" : @(function),
+                                                                                            @"endValue" : @(end),
+                                                                                            @"startValue" : @(start),
+                                                                                            @"duration" : @(time),
+                                                                                            @"progress" : @(0.0)}];
     timer = [NSTimer timerWithTimeInterval:1 / 30.f target:self selector:@selector(updateValue:) userInfo:timingFunction repeats:YES];
     [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 }
 
 - (void)updateValue:(NSTimer *)sender {
-    // Update variables
+    // Get data from timer
     CountingLabelTimingFunction function = (CountingLabelTimingFunction)[(NSNumber *)[[sender userInfo] objectForKey:@"timingFunction"] integerValue];
-    value = [self calculateValue:function];
-    progress += 1 / 30.0f;
+    NSInteger start = [(NSNumber *)[[sender userInfo] objectForKey:@"startValue"] integerValue];
+    NSInteger end = [(NSNumber *)[[sender userInfo] objectForKey:@"endValue"] integerValue];
+    CGFloat duration = [(NSNumber *)[[sender userInfo] objectForKey:@"duration"] floatValue];
+    CGFloat progress = [(NSNumber *)[[sender userInfo] objectForKey:@"progress"] floatValue];
     
-    // Set text
-    self.text = [NSString stringWithFormat:@"%d", (int)value];
+    // Calculate value and set text
+    NSInteger value = [self calculateValueWithTiming:function startValue:start endValue:end duration:duration progress:progress];
+    self.text = [NSString stringWithFormat:@"%ld", value];
+    
+    // Update progress
+    progress += 1 / 30.0f;
+    [[sender userInfo] setObject:@(progress) forKey:@"progress"];
     
     // Stop when value == endvalue
-    if (value == endValue) {
+    if (value == end) {
         [timer invalidate];
     }
 }
 
-- (NSInteger)calculateValue:(CountingLabelTimingFunction)function {
-    Float32 percent = progress / duration;
-    NSInteger delta = endValue - startValue;
-    Float32 result;
+- (NSInteger)calculateValueWithTiming:(CountingLabelTimingFunction)function startValue:(NSInteger)start endValue:(NSInteger)end duration:(CGFloat)time progress:(CGFloat)progress {
+    CGFloat percent = progress / time;
+    NSInteger delta = end - start;
+    CGFloat result;
     switch (function) {
         case CountingLabelTimingFunctionEaseIn:
             // Quintic ease in curve
-            result = delta * pow(percent, 3) + startValue;
+            result = delta * pow(percent, 3) + start;
             break;
         case CountingLabelTimingFunctionEaseOut:
             // Quintic ease out curve
-            result = delta * (pow(percent - 1, 3) + 1) + startValue;
+            result = delta * (pow(percent - 1, 3) + 1) + start;
             break;
         case CountingLabelTimingFunctionEaseInOut:
             // Quintic ease in/out curve
             if (percent / 2 < 1) {
-                result = (delta / 2) * pow(percent / 2, 3) + startValue;
-            } else result = (delta / 2) * (pow(percent / 2, 3) + 2) + startValue;
+                result = (delta / 2) * pow(percent / 2, 3) + start;
+            } else result = (delta / 2) * (pow(percent / 2, 3) + 2) + start;
             break;
     }
     return result;
